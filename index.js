7335c4d5b72aef4daad7641a6a8f83983b3a921b
WarleySantos-cyfer <warlmuta@gmail.com>
2025-11-19 13:08:17 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+require('dotenv').config();
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+client.on('ready', () => {
+    console.log(`Bot online como ${client.user.tag}`);
+});
+
+client.on('messageCreate', msg => {
+    if (msg.content === '!ping') {
+        msg.reply('Pong!');
+    }
+});
+
+client.login(process.env.TOKEN);
